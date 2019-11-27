ab47e6ac57325a4610595ae19518daa670b86a45
Wantonius <hentonen@gmail.com>
2019-11-27 12:37:19 +0200

    led server

---
+++ node/led_server/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fs = require("fs");
+
+let app = express();
+
+app.use(express.static("public"));
+
+app.get("/led/:state", function(req,res) {
+	fs.writeFile("/sys/class/ledclass/led01/led_attr",req.params.state,function(err) {
+		if(err) {
+			return res.status(404).json({message:"Failed to handle led"});	
+			console.log(err);
+			throw err;	
+		}
+		return res.status(200).json({message:"Success"});
+	})
+});
+
+app.listen(3000);
+console.log("Running in port 3000");
